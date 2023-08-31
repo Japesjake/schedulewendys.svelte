a60fccba6d94e5310769cc0ce72d8d5cac670caf
japesjake <japesjake@gmail.com>
2023-08-30 20:16:46 -0400

    added sandwiches checkboxes.

--- src/lib/components/Employees.svelte
+++ src/lib/components/Employees.svelte
@@ -6,28 +6,26 @@
 		$data.employees.splice(id + 1, 0, defaultPerson);
 		$data = $data;
 	}
-	function toggle (obj) {
-		return obj ? false: true
-	}
     function removeInput (id) {
 		$data.employees.splice(id,1);
 		$data = $data
 	}
 	function toggleManager (id) {
-		$data.employees[id].manager = toggle($data.employees[id].manager)
+		$data.employees[id].manager = $data.employees[id].manager ? false: true
 	}
 	function toggleSandwiches (id) {
-		$data.employees[id].sandwiches = toggle($data.employees[id].sandwiches)
+		$data.employees[id].sandwiches = $data.employees[id].sandwiches ? false: true
 	}
 </script>
 <div style='display: flex'>
 <div style='padding-left: 52px;'>Employee:</div>
 <div style='padding-left: 40px;'>Manager?</div>
+<div style='padding-left: 40px;'>Sandwiches?</div>
 </div>
 {#each $data.employees as employee, id (employee.id)}
 <button tabindex = -1 on:click={removeInput(id)}>-</button>
 <button tabindex = -1 on:click={addInput(id)}>+</button>
-<input style='width: 7em;' tabindex = 0 type="text" placeholder="name" bind:value={employee.name}>
+<input tabindex = 0 type="text" style='width: 7em;' placeholder="name" bind:value={employee.name}>
 <input type='checkbox' on:click={toggleManager(id)}>
 <input type='checkbox' on:click={toggleSandwiches(id)}>
 <br>
@@ -35,7 +33,7 @@
 <button on:click={addInput($data.employees.length)}>+</button>
 
 <style>
-	.employee {
-
+	input[type='checkbox'] {
+		margin-left: 2em;
 	}
 </style>
