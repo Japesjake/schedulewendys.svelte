135acb32090ea157d5c53212407f81aa7400b525
Japesjake <japesjake@gmail.com>
2024-01-07 22:10:06 -0500

    Revert "commit before migration and after npm update."
    This reverts commit ee79adc3ad4c3aa40cf301de300452182fd6f74f.

--- src/lib/week.js
+++
@@ -1,8 +0,0 @@
-export default function week () {
-    let daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-    let days = []
-    for (let i = 0; i<daysOfWeek.length; i++) {
-        days.push({name: daysOfWeek[i], startTime: '06:00', endTime: '17:00', assigned: false})
-    }
-    return days
-}
