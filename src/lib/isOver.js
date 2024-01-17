6ce11018f5ffb31740b8ded1e29fee2e8d0020c1
Japesjake <japesjake@gmail.com>
2024-01-17 12:34:46 -0500

    in the process of fixing a bug where off days wont switch to on after employee hours are less than max.

--- src/lib/isOver.js
+++ src/lib/isOver.js
@@ -3,12 +3,12 @@ import timeToMinutes from "./timeToMinutes"
 
 export default function isOver(emp) {
     let totalMinutes = 0
-    for (let day of emp.days) {
-
+    for (let day of emp.days) { 
         let start = timeToMinutes(day.startTime)
         let end = timeToMinutes(day.endTime)
         let minutes = end - start
         totalMinutes += minutes
+        console.log(totalMinutes)
     }
     if (totalMinutes/60 > emp.max) {
         return true
--- src/lib/toggleAvailable.js
+++ src/lib/toggleAvailable.js
@@ -2,11 +2,15 @@ export default function toggleAvailable(employees) {
     for (let emp of employees) {
         for (let day of emp.days) {
             if (day.available == false) {
+                day.startTimeStore = day.startTime
+                day.endTimeStore = day.endTime
                 day.startTime = 'off'
                 day.endTime = 'off'
-            } else {
-                day.startTime = '06:00'
-                day.endTime = '15:00'
+            } else if (day.startTimeStore) {
+                day.startTime = day.startTimeStore
+                day.endTime = day.endTimeStore
+                day.startTimeStore = false
+                day.endTimeStore = false
             }
         }
     }
