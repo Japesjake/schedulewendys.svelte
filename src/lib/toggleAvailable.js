198d4cf4c0f59dc3b937d43269d582bc9ae28dd0
japesjake <japesjake@gmail.com>
2024-01-20 03:03:26 -0500

    I have appeared to fix bug where time didnt switch back when availablility was toggle and when the hours were below max

--- src/lib/toggleAvailable.js
+++ src/lib/toggleAvailable.js
@@ -1,12 +1,16 @@
+import $data from '$lib/stores.js'
 export default function toggleAvailable(employees) {
     for (let emp of employees) {
         for (let day of emp.days) {
-            if (day.available == false) {
+            if (day.available == false && !day.startTimeStore) {
                 day.startTimeStore = day.startTime
                 day.endTimeStore = day.endTime
                 day.startTime = 'off'
                 day.endTime = 'off'
-            } else if (day.startTimeStore) {
+            } else if (day.available == false && day.startTimeStore) {
+                day.startTime = 'off'
+                day.endTime = 'off'
+            } else if (day.available == true && day.startTimeStore) {
                 day.startTime = day.startTimeStore
                 day.endTime = day.endTimeStore
                 day.startTimeStore = false
@@ -14,4 +18,5 @@ export default function toggleAvailable(employees) {
             }
         }
     }
+    $data.employees = employees
 }
