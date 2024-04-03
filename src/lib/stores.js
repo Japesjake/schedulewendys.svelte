60ab9c2a1554254bd1e1bccf204b483752bdfa7c
japesjake <japesjake@gmail.com>
2024-04-03 03:55:35 -0400

    changed default store to relevant employees for 13290

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -3,7 +3,7 @@ import { browser } from '$app/environment'
 import Employee from './classes/Employee.js'
 import Restaurant from './classes/Restaurant.js'
 
-let data = new Restaurant('Sarasota', [new Employee('John Jaquith'), new Employee('Evan')]);
+let data = new Restaurant('Sarasota', [new Employee('John Jaquith'), new Employee('Hannah'), new Employee('Sharon')]);
 let initialValue = browser ? JSON.parse(localStorage.getItem('data')) ?? data : data;
 data = writable(initialValue)
 data.subscribe((value) => {
